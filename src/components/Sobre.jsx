f68b14b02e2c2cb0f2da08be0726807bab9e6d31
RenatoFBabi <166869030+RenatoFBabi@users.noreply.github.com>
2024-09-07 22:37:38 -0300

    Update Sobre.jsx - Importando as informações de nosso produto

--- src/components/Sobre.jsx
+++ src/components/Sobre.jsx
@@ -1,7 +1,9 @@
 const Sobre=()=>{
     return(
         <>
-        
+        <h3>Nosso site oferece preços justos e competitivos, trazendo a melhor qualidade e oportunidades para nossos clientes! <br/>
+            Obrigado por optar por nossos serviços!
+        </h3>
         </>
     )
 }
