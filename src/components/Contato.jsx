f223d8e8da78ee9abea24962b249a137420207a2
RenatoFBabi <166869030+RenatoFBabi@users.noreply.github.com>
2024-09-07 22:36:37 -0300

    Update Contato.jsx - Adicionando informações necessárias

--- src/components/Contato.jsx
+++ src/components/Contato.jsx
@@ -1,7 +1,13 @@
 const Contato=()=>{
     return(
         <>
-        
+                <h3>Bem-vindo ao SportMax!
+<br/>
+Obrigado por usar nossos produtos! Estamos felizes em tê-lo conosco.
+Para qualquer dúvida ou suporte, entre em contato conosco pelo e-mail: <strong>suporte@sportmax.com.</strong>
+Atenciosamente,
+Equipe SportMax.
+</h3>
         </>
     )
 }
